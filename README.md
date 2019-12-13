b74762707576281f7891547249790db4797b432c
bjarkirafn <bjarkirafn@gmail.com>
2019-12-13 00:46:28 +0000

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+# sapper-tailwindcss-starter-minimal template
+#### The purpose of this template is to keep it as close to the original Sapper template as possible
+
+@apply can be used in both css files and .svelte style tags.
+
+
+```bash
+#to install
+npx degit bjarkirafn/sapper-tailwind-starter my-app
+cd my-app
+npm install # or yarn!
+npm run dev
+```
+
+Open up [localhost:3000](http://localhost:3000) and start clicking around.
+
+Consult [sapper.svelte.dev](https://sapper.svelte.dev) for help getting started.
+
+
+There's no purgecss. Instructions may be added.
+
+#### To enable more plugins, simply install depencies and extend postcssPlugins
+```javascript
+const postcssPlugins = [
+    require("postcss-import")(),
+    require("postcss-url")(),
+    require("tailwindcss")("./tailwind.config.js"),
+    require("autoprefixer")({ browsersList: ["defaults"] })
+    ...
+]
+```
---
+++ src/routes/index.svelte
@@ -0,0 +1,60 @@
+<style>
+  h1,
+  figure,
+  p {
+    text-align: center;
+    margin: 0 auto;
+  }
+
+  h1 {
+    font-size: 2.8em;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+    @apply text-green-500;
+  }
+
+  figure {
+    margin: 0 0 1em 0;
+  }
+
+  img {
+    width: 100%;
+    max-width: 400px;
+    margin: 0 0 1em 0;
+  }
+
+  p {
+    margin: 1em auto;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
+  .svelte-style {
+    @apply shadow-2xl text-center rounded-full;
+  }
+</style>
+
+<svelte:head>
+  <title>Sapper project template</title>
+</svelte:head>
+
+<h1>Great success!</h1>
+<div
+  class="bg-from-utils-css svelte-style p-5 text-white text-xl font-semibold">
+  TAILWIND
+</div>
+
+<figure class="flex items-center flex-col">
+  <img class="p-4" alt="Borat" src="great-success.png" />
+  <figcaption>HIGH FIVE!</figcaption>
+</figure>
+
+<p>
+  <strong>
+    Try editing this file (src/routes/index.svelte) to test live reloading.
+  </strong>
+</p>
